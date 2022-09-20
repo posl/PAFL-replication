f9789c0f3a8fe587847208ad5eb2810a50667bcc
y629 <yuutaishimoto@gmail.com>
2022-09-20 17:05:11 -0600

    update extract_original_trace

--- PAFL/extract_pfa/extract_original_trace/do_ori_extract.py
+++ PAFL/extract_pfa/extract_original_trace/do_ori_extract.py
@@ -1,7 +1,9 @@
 import os
 import sys
 import glob
+import argparse
 import torch
+import numpy as np
 sys.path.append("../../") # extract_pfa/extract_original_traceをインポートするために必要
 sys.path.append("../../../") # model_utilsをインポートするために必要
 # 同階層のextract_original_traceからインポート
@@ -13,20 +15,28 @@ from utils.constant import *
 
 if __name__=="__main__":
   # コマンドライン引数でデータセットやモデルタイプ,GPUの情報を入力
-  dataset = sys.argv[1]
-  isTomita = True if dataset.isdigit() else False
-  model_type = sys.argv[2]
-  use_clean = int(sys.argv[3])
+  parser = argparse.ArgumentParser()
+  parser.add_argument("dataset", type=str, help='abbrev. of datasets')
+  parser.add_argument("model_type", type=str, help='type of models')
+  parser.add_argument("--device", type=str, help='cpu or id of gpu', default='cpu') # gpuならその番号(0,1,...)
+  parser.add_argument("--use_clean", type=bool, help='filter stop word or not', default=True)
+  parser.add_argument("--start_boot_id", type=int, help='What id of bootstrap sample starts training from.', default=0)
+  args = parser.parse_args()
+
+  dataset, model_type = args.dataset, args.model_type
   device = "cuda:0" if torch.cuda.is_available() else "cpu"
-  # device = 'cpu'
+  isTomita = True if dataset.isdigit() else False
   #  number of bootstrap samplings
   B = 10
 
-  for i in range(B):
+  elapsed_times = np.zeros((B))
+  for i in range(args.start_boot_id, B):
     print("========= use bootstrap sample {} for training data =========".format(i))
     # モデルファイルが含まれるディレクトリ名を取得
     model_dir = os.path.join(getattr(getattr(TrainedModel, model_type.upper()), dataset.upper()), "boot_{}".format(i)) if not isTomita else \
       os.path.join(getattr(TrainedModel, model_type.upper()).TOMITA.format(dataset), "boot_{}".format(i))
     load_model_path = glob.glob(get_path(os.path.join(model_dir, "*.pkl")))[0]
     # 隠れ状態のトレースをとる
-    make_ori_trace(model_type, dataset, device, load_model_path, i, use_clean=use_clean)
+    elapsed_times[i] = make_ori_trace(model_type, dataset, device, load_model_path, i, use_clean=args.use_clean)
+    print(f'elapsed for trace extraction: {elapsed_times[i]} [sec.]')
+  print(f'mean epalsed for trace extraction in {B} boots: {np.mean(elapsed_times)} [sec.]')
--- PAFL/extract_pfa/extract_original_trace/extract_ori_trace.py
+++ PAFL/extract_pfa/extract_original_trace/extract_ori_trace.py
@@ -1,3 +1,4 @@
+import time
 import sys
 sys.path.append("../../../") # utilsをインポートするために必要
 # 異なる階層のmodel_utilsからインポート
@@ -5,6 +6,7 @@ from model_utils.model_util import sent2tensor, load_model, get_model_file
 # 異なる階層のutilsからインポート
 from utils.help_func  import load_pickle, save_pickle, filter_stop_words
 from utils.constant import *
+import torch
 
 def make_ori_trace(model_type, dataset, device, load_model_path, boot_id, use_clean=False, data=None, save_path=None):
   """
@@ -25,25 +27,34 @@ def make_ori_trace(model_type, dataset, device, load_model_path, boot_id, use_cl
     何番目のbootstrap sampleか (0-indexed)
   use_clean: bool
     Trueの場合, filter_stop_wordsする
+
+  Return 
+  ------------------
+  elapsed: float
+  トレース取得にかかった時間[sec.]を返す．
   """
   isTomita = True if dataset.isdigit() else False
   # {boot_id}番目のbootstrap sampleをロード
   print("load data...")
   if data is None:
     data = load_pickle(os.path.join(get_path(getattr(DataPath, dataset.upper()).BOOT_DATA_DIR), "boot_{}.pkl".format(boot_id))) if not isTomita else \
-      load_pickle(get_path(DataPath.TOMITA.SPLIT_DATA.format(dataset)))
+      load_pickle(os.path.join(get_path(DataPath.TOMITA.BOOT_DATA_DIR.format(dataset)), f'boot_{boot_id}.pkl'))
   # 前処理済みのデータをロードしてくる
   ori_data = load_pickle(get_path(getattr(DataPath, dataset.upper()).SPLIT_DATA)) if not isTomita else \
     load_pickle(get_path(DataPath.TOMITA.SPLIT_DATA.format(dataset)))
-  word2idx = ori_data["word_to_idx"]
-  # word2vecにかけた後のデータをロードしてくる
-  wv_matrix = load_pickle(get_path(getattr(DataPath, dataset.upper()).WV_MATRIX)) if not isTomita else \
-    load_pickle(get_path(DataPath.TOMITA.WV_MATRIX.format(dataset)))
+  if dataset != DataSet.MNIST:
+    word2idx = ori_data["word_to_idx"]
+    # word2vecにかけた後のデータをロードしてくる
+    wv_matrix = load_pickle(get_path(getattr(DataPath, dataset.upper()).WV_MATRIX)) if not isTomita else \
+      load_pickle(get_path(DataPath.TOMITA.WV_MATRIX.format(dataset)))
+  
   # 入力する埋め込みベクトルの次元(データセットごとに異なる)
   if dataset == DataSet.BP:
     input_dim = 29
-  elif dataset == DataSet.MR or dataset == DataSet.IMDB:
+  elif dataset == DataSet.MR or dataset == DataSet.IMDB or dataset == DataSet.TOXIC:
     input_dim = 300
+  elif dataset == DataSet.MNIST:
+    input_dim = 28
   else: # tomita
     input_dim = 3
 
@@ -58,12 +69,17 @@ def make_ori_trace(model_type, dataset, device, load_model_path, boot_id, use_cl
   ori_traces["train_pre_y"] = []
   ori_traces["test_pre_y"] = []
   
+  s_time = time.perf_counter()
   # 訓練データに対するoriginal trace取得
   print("get ori_trace of model for train set...")
   for x in data["train_x"]:
-    if use_clean:
-      x = filter_stop_words(x)
-    tensor_sequence = sent2tensor(x, input_dim, word2idx, wv_matrix, device)
+    if dataset == DataSet.MNIST:
+      tensor_sequence = torch.unsqueeze(torch.tensor(x), 0) / 255 # (1, 28, 28)
+    else:
+      if use_clean:
+        x = filter_stop_words(x)
+      tensor_sequence = sent2tensor(x, input_dim, word2idx, wv_matrix, device)
+    tensor_sequence = tensor_sequence.to(device)
     hn_trace, label_trace = model.get_predict_trace(tensor_sequence)
     # 途中の隠れ状態のトレースをリストに追加する
     ori_traces["train_x"].append(hn_trace)
@@ -88,3 +104,6 @@ def make_ori_trace(model_type, dataset, device, load_model_path, boot_id, use_cl
       os.path.join(get_path(getattr(OriTrace, model_type.upper()).TOMITA.format(dataset)), "boot_{}.pkl".format(boot_id))
   save_pickle(save_path, ori_traces)
   print("original trace saved to {}\n".format(save_path))
+  f_time = time.perf_counter()
+  elapsed = f_time - s_time
+  return elapsed
